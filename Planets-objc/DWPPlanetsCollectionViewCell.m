e4c00f31a7227fc0da5aa325ba317dcc0918bb8e
Dongwoo Pae <dongwoopae@gmail.com>
2019-10-31 10:51:17 +0900

    completed part 1 - storyboard setup

--- Planets-objc/DWPPlanetsCollectionViewCell.m
+++ Planets-objc/DWPPlanetsCollectionViewCell.m
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  DWPPlanetsCollectionViewCell.m
 //  Planets-objc
 //
 //  Created by Dongwoo Pae on 10/31/19.
 //  Copyright © 2019 Dongwoo Pae. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "DWPPlanetsCollectionViewCell.h"
 
-@interface ViewController : UIViewController
-
+@implementation DWPPlanetsCollectionViewCell
 
 @end
-
---
+++ Planets-objc/DWPPlanetsCollectionViewController.h
@@ -0,0 +1,17 @@
+//
+//  DWPPlanetsCollectionViewController.h
+//  Planets-objc
+//
+//  Created by Dongwoo Pae on 10/31/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DWPPlanetsCollectionViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
